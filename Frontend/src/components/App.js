2b94e59d69547ca1c52eadcb780f756d90c97937
Rodolfo-LA <lopez.rodolfo@neuf.fr>
2022-10-06 15:15:58 +0200

    Supression des routes inutiles dans App.js

--- Frontend/src/components/App.js
+++ Frontend/src/components/App.js
@@ -6,8 +6,6 @@ import Connexion from './Connexion'
 import Footer from './Footer'
 import Envoi from './Envoi'
 import Premiere from './Premiere'
-import CreatePost from './CreatePost'
-import DisplayPost from './DisplayPost'
 
 function App() {
     return (
@@ -18,8 +16,6 @@ function App() {
                 <Route path="/ins" element={<Inscription/>} />
                 <Route path="/con" element={<Connexion/>} />
                 <Route path="/env/:token/:userId" element={<Envoi/>} />
-                {/*<Route path="/create" element={<CreatePost/>} />
-                <Route path="/display" element={<DisplayPost/>} /> */}
             </Routes>
             <Footer />
         </div>
